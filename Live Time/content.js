fe7d889e5f124c52a658236f8074fbf9c7816bb3
raghav <raghavrakh@gmail.com>
2020-09-04 15:04:43 +0530

    Boilerplate

---
+++ Live Time/content.js
@@ -0,0 +1,12 @@
+console.log("Live Time Chrome Extension");
+
+
+
+chrome.runtime.onMessage.addListener(gotMessage);
+function gotMessage(message,sender,sendResponse)
+{
+  console.log(message.txt);
+  if(message.txt==="hello"){
+    ///the function which we want to  happen to the content
+  }
+}
