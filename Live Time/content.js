c9726587fdfb7de7e0a0da2cc91c38c323eb7b7e
raghav <raghavrakh@gmail.com>
2020-09-05 18:17:30 +0530

    icon added

--- Live Time/content.js
+++ Live Time/content.js
@@ -1,12 +1,9 @@
 console.log("Live Time Chrome Extension");
-
-
-
 chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message,sender,sendResponse)
 {
   console.log(message.txt);
   if(message.txt==="hello"){
-    ///the function which we want to  happen to the content
+    ///the function which we want to  happen to the content section
   }
 }
