b76aaec51d5a425a21b4be6df6850b8402fa3a63
raghav <raghavrakh@gmail.com>
2020-09-06 11:13:54 +0530

    content security

--- Live Time/content.js
+++ Live Time/content.js
@@ -1,9 +1,10 @@
 console.log("Live Time Chrome Extension");
+
 chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message,sender,sendResponse)
 {
   console.log(message.txt);
-  if(message.txt==="hello"){
-    ///the function which we want to  happen to the content section
-  }
+  // if(message.txt==="hello"){
+  //   ///the function which we want to  happen to the content section
+  // }
 }
